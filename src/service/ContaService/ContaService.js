a915d45db7b4859d23ef9c52f3ae20f60ad15c98
Igor Venancio <igor@investtools.com.br>
2023-04-24 21:23:57 -0300

    ContaService

--- src/service/ContaService/ContaService.js
+++ src/service/ContaService/ContaService.js
@@ -1,20 +1,52 @@
 class ContaService {
-    cadastrarBanco(nomeBanco) {
-        return fetch('http://127.0.0.1:5000/api/bancos/cadastrarBanco', {
+
+    abrirConta(cpfCliente, nomeBanco, sd, numeroConta) {
+        return fetch('http://127.0.0.1:5000"/api/contas/abrirContas', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ nome: nomeBanco })
+            body: JSON.stringify({ cpf: cpfCliente, nome_banco: nomeBanco, saldo: sd, conta: numeroConta })
         })
             .then(response => response.json())
             .then(data => console.log(data))
             .catch(error => console.error(error));
     }
 
-    getAllBancos() {
-        return fetch('http://127.0.0.1:5000/api/bancos/')
+    transferirValorEntreContas(idOrigem, idDestino, cpfCliente, valorOrigem) {
+        return fetch('http://127.0.0.1:5000/api/contas/transferirValorEntreContas', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ id_origem: idOrigem, id_destino: idDestino, cpf: cpfCliente, valor_origem: valorOrigem })
+        })
             .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+    }
+
+    getContasPorCliente(cpf) {
+        return fetch(`http://127.0.0.1:5000/api/contas/getContasPorCliente?cpf=${cpf}`)
+            .then(response => response.json())
+            .catch(error => console.error(error));
+    }
+    getSaldoTotalContas(cpf) {
+        return fetch(`http://127.0.0.1:5000/api/contas/getSaldoTotalContas?cpf=${cpf}`)
+            .then(response => response.json())
+            .catch(error => console.error(error));
+    }
+
+    deletarConta(cpfCliente, idConta) {
+        return fetch(`http://127.0.0.1:5000/api/contas/deletarConta?id_conta=${idConta}&cpf=${cpfCliente}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ cpf: cpfCliente, id_conta: idConta })
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
             .catch(error => console.error(error));
     }
 }
